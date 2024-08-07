93bce7d451fe167cdede2a3bcec5464e5a8a2310
Bradleyh212 <bdesm052@uottawa.ca>
2024-08-07 00:50:51 -0400

    few changes, adding the login button

--- Reconsil.py
+++ Reconsil.py
@@ -10,9 +10,9 @@ window.title("Narcotics Management System")
 window.geometry('800x600')
 
 greeting = tk.Label(
-	text="Narc Management System",
+	text="Welcome back",
 	foreground = "white", # Set the text color
-	background = "black", # Set the text background color
+	#background = "black", # Set the text background color
 	width = 20,
 	height = 1
 
@@ -22,12 +22,15 @@ greeting = tk.Label(
 
 login = tk.Button(
 	text = "Sign In",
-	bg = "orange",
-	fg = "black"
-
+	bg = "black",
+	fg = "white",
+	width = 20,
+	height = 15
 	)
 
 greeting.pack()
+
+login.pack()
 
 
 narc_list = {"upc":["Sandoz-Amphetamine Xr", "5mg", "02457288", "ER Cap", "100"]} #example of first medication
