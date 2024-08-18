d8d3d5c315f72813f7e952155401562af17fab02
Bradleyh212 <bdesm052@uottawa.ca>
2024-08-18 00:05:02 -0400

    Deleted Reconsil.py, added main_page.py, modified meds.py, i can now lookup the meds when entering a certain upc

---
+++ main_page.py
@@ -0,0 +1,45 @@
+#main page, will be full screen, not reziable
+import tkinter as tk
+from tkinter import ttk
+from tkinter import font
+from tkinter import messagebox
+from meds import narc_list 
+
+main_page_window = tk.Tk()
+main_page_window.title("Narcotics Management System")
+
+#Window setting
+main_page_window.state('zoomed') #setting window to full screen before stopping the resizable to false
+
+#main_page_window.resizable(False, False) #This stops the user from resizing the screen for the login ui
+
+
+#end of main_page_window setting
+
+def search_meds():
+	if upc_ent.get() in narc_list:
+		name_lbl_output.config(text = narc_list[upc_ent.get()])
+
+
+# Define a font for the Entry widget
+font = font.Font(family="Inter", size=16, weight="normal")
+
+upc_ent = tk.Entry(fg = "black", bg = "white", width = 70, font = font) #upc entry widget
+upc_ent.pack(pady = 20)
+
+search_btn = ttk.Button(text = "Search", style='TButton', command=search_meds)
+search_btn.pack()
+
+
+
+name_lbl_output = tk.Label(text = "Allo", bg = "white", fg = "red", width = 70, font = font)
+name_lbl_output.pack() # this will be the label to test the output when we enter the upc
+
+
+
+
+
+
+#Running the program
+
+main_page_window.mainloop()
--- meds.py
+++ meds.py
@@ -2,8 +2,8 @@ import sqlite3
 con = sqlite3.connect("tutorial.db")
 
 
-narc_list = {"upc":["Sandoz-Amphetamine Xr", "5mg", "02457288", "ER Cap", "100"]} #example of first medication
+narc_list = {"0123456789":["Sandoz-Amphetamine Xr", "5mg", "02457288", "ER Cap", "100"]} #example of first medication
 
-print(narc_list["upc"])
+
 
 
