18859552e47032f41b0e40ba061090243b60c710
Bradleyh212 <bdesm052@uottawa.ca>
2024-08-24 13:51:35 -0400

    adding more products to the excel sheet

--- main_page.py
+++ main_page.py
@@ -20,16 +20,18 @@ def open_main_page():
 
 	#end of main_page_window setting
 	def search(search):
-		search_meds()
+		search_meds_by_upc()
 	main_page_window.bind('<Return>', search)
 
 
-	def search_meds(): #function to find the meds in meds.py
+	def search_meds_by_upc(): #function to find the meds in meds.py
 		upc = upc_ent.get()
 		if upc in narc_list:
 			med_info = narc_list[upc]
 			name_lbl_output.config(text = med_info[0])
-			strength_lbl_output.config(text = med_info[1])
+			din__med_output.config(text = med_info[1])
+			strength_lbl_output.config(text = med_info[2])
+			pack_med_output.config(text = med_info[3])
 		else:
 			messagebox.showerror("Error", "UPC not found")
 
@@ -41,7 +43,7 @@ def open_main_page():
 	upc_ent.pack(pady = 20)
 	upc_ent.focus()
 
-	search_btn = ttk.Button(main_page_window, text = "Search", style='TButton', command=search_meds)
+	search_btn = ttk.Button(main_page_window, text = "Search", style='TButton', command=search_meds_by_upc)
 	search_btn.pack()
 
 	name__med_lbl = tk.Label(main_page_window, text = "name", bg = "white", fg = "black", width = 20, font = font)
@@ -50,13 +52,23 @@ def open_main_page():
 	name_lbl_output = tk.Label(main_page_window, bg = "white", fg = "red", width = 70, font = font)
 	name_lbl_output.pack() # this will be the label to test the output when we enter the upc
 
+	din__med_lbl = tk.Label(main_page_window, text = "din", bg = "white", fg = "black", width = 20, font = font)
+	din__med_lbl.pack(pady = 10)
+
+	din__med_output = tk.Label(main_page_window, bg = "white", fg = "red", width = 70, font = font)
+	din__med_output.pack()
+
 	strength__med_lbl = tk.Label(main_page_window, text = "strength", bg = "white", fg = "black", width = 20, font = font)
 	strength__med_lbl.pack(pady = 10)
 
 	strength_lbl_output = tk.Label(main_page_window, bg = "white", fg = "red", width = 70, font = font)
 	strength_lbl_output.pack()
 
+	pack_med_lbl = tk.Label(main_page_window, text = "pack size", bg = "white", fg = "black", width = 20, font = font)
+	pack_med_lbl.pack(pady = 10)
 
+	pack_med_output = tk.Label(main_page_window, bg = "white", fg = "red", width = 70, font = font)
+	pack_med_output.pack()
 
 
 
--- meds.py
+++ meds.py
@@ -4,24 +4,33 @@ import pandas as pd
 
 narc_list = {}
 df = pd.read_excel("Sheet1.xlsx", sheet_name="Sheet1")
-upc_list = df["Upc"].fillna(1).apply(lambda x: str(int(x)).zfill(12)).tolist()
+upc_list = df["Upc"].fillna(1).apply(lambda x: str(int(x)).zfill(12)).tolist() #make the upc list, also fill the first numbers with zero
 #print(upc_list)
 
 drug_name_list = df["Drug Name"].tolist()
 #print(drug_name_list)
 
+drug_din_list = df["DIN"].apply(lambda x: str(int(x)).zfill(8)).tolist() #make the din list, also fill the first numbers with zero
+#print(drug_din_list)
+
 drug_stregth_list = df["Strength"].tolist()
 #print(drug_stregth_list)
 
+drug_pack_size_list = df["Pack size"].fillna(1).astype(int).tolist()
+#print(drug_pack_size_list)
+
 
 for i in range(len(upc_list)):
-	narc_list[upc_list[i]] = [drug_name_list[i], drug_stregth_list[i]]
-
+		narc_list[upc_list[i]] = [drug_name_list[i], drug_din_list[i], drug_stregth_list[i], drug_pack_size_list[i]]
 
 
 
 print()
-print(narc_list)
+print(len(narc_list))
+print()
+print(narc_list["057606781840"])
+print(narc_list["057606781840"])
+
 
 
 
