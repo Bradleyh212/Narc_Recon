ddf74de71012b546358a68a8359f90a75e8ab86b
Bradleyh212 <bdesm052@uottawa.ca>
2024-09-09 11:33:13 -0400

    finish adding all widgets main page

--- main_page.py
+++ main_page.py
@@ -21,9 +21,79 @@ main_page_window.geometry('%dx%d' % (w, h))
 
 #main_page_window.resizable(False, False) #This stops the user from resizing the screen for the login ui
 
+
+
+def search(search):
+    search_narcs()
+main_page_window.bind('<Return>', search)
+
+def search_narcs(): #function to find the meds in meds.py
+    search_input = meds_ent.get()
+    if len(search_input) == 12:
+        tup = find_narcs_upc(search_input)
+    elif len(search_input) == 8:
+        tup = find_narcs_din(search_input)
+    else:
+        messagebox.showerror("Error", "Drug not found")
+        remove_qty_ent.focus_set() #This brings the focus out of the med entry
+        meds_ent.focus_set() # This brings back the focus to med entry
+        return
+
+    if len(tup) == 1:
+#There will always be only 1 tuple in the list when looking with upc, will but another constraint, "if len(din) > 1" when lookin with din
+        name_lbl_output.config(text = tup[0][1])
+        din__med_output.config(text = tup[0][0])
+        strength_lbl_output.config(text = tup[0][4])
+        drug_form_output.config(text = tup[0][5])
+        pack_med_output.config(text = tup[0][6])
+        qty_med_output.config(text = find_quantity(tup[0][3])) #functions from the meds file to find the qty directly from the database
+    elif len(tup) > 1:
+        #I will output a choice for which pack size they want
+        choice_windw = tk.Toplevel(main_page_window)
+        choice_windw.title("Choose Pack Size")
+        #Creating the place and the size of the window
+        w = 400 
+        h = 100
+
+        window_width = choice_windw.winfo_screenwidth()  # screen centering code from https://stackoverflow.com/questions/14910858/how-to-specify-where-a-tkinter-window-opens
+        window_height = choice_windw.winfo_screenheight()
+
+        x = (window_width/2) - (w/2)
+        y = (window_height/2) - (h/0.1)
+        choice_windw.geometry('%dx%d+%d+%d' % (w, h, x, y))
+
+        choice_windw.wm_attributes("-topmost", True) #Will keep the choice_windw on top
+        tk.Label(choice_windw, text="Choose the pack size:").pack()
+
+        pack_size = tk.StringVar()
+        pack_size_dropdown = ttk.Combobox(choice_windw, textvariable=pack_size)
+        pack_size_dropdown['values'] = [f"{item[6]} units - {item[4]} {item[5]}" for item in tup]
+        pack_size_dropdown.pack()
+
+        def on_select_pack_size():
+            global selected_pack
+            selected_index = pack_size_dropdown.current()
+            selected_pack = tup[selected_index]
+            name_lbl_output.config(text=selected_pack[1])
+            din__med_output.config(text=selected_pack[0])
+            strength_lbl_output.config(text=selected_pack[4])
+            drug_form_output.config(text=selected_pack[5])
+            pack_med_output.config(text=selected_pack[6])
+            qty_med_output.config(text=find_quantity(selected_pack[3]))
+            print(selected_pack)
+            choice_windw.destroy()
+
+        select_btn = tk.Button(choice_windw, text="Select", command=on_select_pack_size)
+        select_btn.pack()
+    else:
+        messagebox.showerror("Error", "Drug not found")
+        remove_qty_ent.focus_set() #This brings the focus out of the med entry
+        meds_ent.focus_set() # This brings back the focus to med entry
+
+
 # Creating the fonts
 header_font = font.Font(family="Inter", size=70, weight="normal")
-font = font.Font(family="Inter", size=35, weight="normal") # Define a font for the Entry widget
+font = font.Font(family="Inter", size=30, weight="normal") # Define a font for the Entry widget
 # The size of the text changes the height of the Entry widget
 
 # Creating the frames
@@ -39,7 +109,6 @@ left_body_frame = tk.Frame(body_frame, width = w-500, height = h, bg = "orange")
 left_body_frame.grid(row = 0, column = 0)
 left_body_frame.grid_propagate(False) # Prevent the left_body_frame frame from resizing based on its content
 
-
 right_body_frame = tk.Frame(body_frame, width = 500, height = h, bg = "green")
 right_body_frame.grid(row = 0, column = 1)
 right_body_frame.grid_propagate(False) # Prevent the right_body_frame frame from resizing based on its content
@@ -48,6 +117,22 @@ nav_frame = tk.Frame(header_frame, width = 450, height = 100, bg = "purple")
 nav_frame.grid(row = 0, column = 1, padx = 550)
 nav_frame.grid_propagate(False) # Prevent the nav frame from resizing based on its content
 
+picture_frame = tk.Frame(right_body_frame, width = 200, height = 200, bg = "Black")
+picture_frame.grid(padx = 150, pady = 100)
+picture_frame.grid_propagate(False) # Prevent the picture frame from resizing based on its content
+
+
+def on_entry_click(event): # When the med entry widget is clicked remove the text
+	"""Remove placeholder when entry is clicked."""
+	if meds_ent.get() == 'Enter DIN or UPC':
+		meds_ent.delete(0, "end")  # Delete all the text in the entry
+		meds_ent.config(fg='white')  # Change text color to white
+
+def on_focusout(event): # When the med entry widget is un-clicked add the text back
+	"""Re-add placeholder if entry is empty when focus is lost."""
+	if meds_ent.get() == '':
+		meds_ent.insert(0, 'Enter DIN or UPC')
+		meds_ent.config(fg='gray')
 
 def refresh_page():
 	global meds_ent, name_lbl_output, din__med_output, strength_lbl_output, drug_form_output, pack_med_output, qty_med_output, remove_qty_ent
@@ -65,50 +150,38 @@ def refresh_page():
 	log_off_btn = ttk.Button(nav_frame, text = "LOGOUT", style='TButton', command = lambda : [main_page_window.destroy()]) 
 	log_off_btn.grid(row = 0, column = 2, padx = 30, ipady=0, ipadx=0)
 
-	meds_ent = tk.Entry(left_body_frame, text = "Enter upc or din", fg = "white", bg = "black", width = 40, font = font, justify="center") #upc entry widget
-	meds_ent.grid(row = 0, column = 0, pady = 20, padx = 20)
+	meds_ent = tk.Entry(left_body_frame, text = "Enter upc or din", fg = "white", bg = "black", width = 35, font = font, justify="center") #upc entry widget
+	meds_ent.grid(row = 0, column = 0, pady = 40, padx = 20, )
 	meds_ent.focus()
+	meds_ent.bind('<FocusIn>', on_entry_click)
+	meds_ent.bind('<FocusOut>', on_focusout)
+
+	search_btn = ttk.Button(left_body_frame, text = "Search", style='TButton', command=search_narcs)
+	search_btn.grid(row = 0, column = 1, padx = 10, sticky = "e")
+
+	name_lbl_output = tk.Label(left_body_frame, bg = "black", fg = "White", width = 19, font = font)
+	name_lbl_output.grid(row = 1, column = 0, padx = 60, pady = 0, sticky = "w") # this will be the label to see the output when we enter the upc or din
+
+	din__med_output = tk.Label(left_body_frame, bg = "black", fg = "red", width = 12, font = font)
+	din__med_output.grid(row = 1, column = 1, padx = 0, sticky = "w")
+
+
+	strength_lbl_output = tk.Label(left_body_frame, bg = "black", fg = "red", width = 19, font = font)
+	strength_lbl_output.grid(row = 2, column = 0, padx = 60, pady = 30, sticky = "w")
+
+	drug_form_output = tk.Label(left_body_frame, bg = "black", fg = "red", width = 12, font = font)
+	drug_form_output.grid(row = 2, column = 1, padx = 0, sticky = "w")
+
+	pack_med_output = tk.Label(left_body_frame, bg = "black", fg = "red", width = 20, font = font)
+	pack_med_output.grid(row = 3)
+
+	med_picture = tk.Label(picture_frame, text = "PICTURE", bg = "black", fg = "white", width = 10, font = font)
+	med_picture.grid(pady = 75)
+
+	qty_med_output = tk.Label(right_body_frame, bg = "black", fg = "red", width = 10, font = font)
+	qty_med_output.grid(row = 1)
 
 	'''
-	search_btn = ttk.Button(main_page_window, text = "Search", style='TButton', command=search_narcs)
-	search_btn.pack()
-
-	name__med_lbl = tk.Label(main_page_window, text = "name", bg = "black", fg = "white", width = 20, font = font)
-	name__med_lbl.pack(pady = 10)
-
-	name_lbl_output = tk.Label(main_page_window, bg = "black", fg = "red", width = 70, font = font)
-	name_lbl_output.pack() # this will be the label to test the output when we enter the upc
-
-	din__med_lbl = tk.Label(main_page_window, text = "din", bg = "black", fg = "white", width = 20, font = font)
-	din__med_lbl.pack(pady = 10)
-
-	din__med_output = tk.Label(main_page_window, bg = "black", fg = "red", width = 70, font = font)
-	din__med_output.pack()
-
-	strength__med_lbl = tk.Label(main_page_window, text = "strength", bg = "black", fg = "white", width = 20, font = font)
-	strength__med_lbl.pack(pady = 10)
-
-	strength_lbl_output = tk.Label(main_page_window, bg = "black", fg = "red", width = 70, font = font)
-	strength_lbl_output.pack()
-
-	drug_form_lbl = tk.Label(main_page_window, text = "Form", bg = "black", fg = "white", width = 20, font = font)
-	drug_form_lbl.pack(pady = 10)
- 
-	drug_form_output = tk.Label(main_page_window, bg = "black", fg = "red", width = 70, font = font)
-	drug_form_output.pack()
-
-	pack_med_lbl = tk.Label(main_page_window, text = "pack size", bg = "black", fg = "white", width = 20, font = font)
-	pack_med_lbl.pack(pady = 10)
-
-	pack_med_output = tk.Label(main_page_window, bg = "black", fg = "red", width = 70, font = font)
-	pack_med_output.pack()
-
-	qty_med_lbl = tk.Label(main_page_window, text = "Qty on hand", bg = "black", fg = "white", width = 20, font = font)
-	qty_med_lbl.pack(pady = 10)
-
-	qty_med_output = tk.Label(main_page_window, bg = "black", fg = "red", width = 70, font = font)
-	qty_med_output.pack()
-
 	remove_qty_ent = ttk.Entry(main_page_window, text = "Fill")
 	remove_qty_ent.pack()
 	remove_qty_btn = ttk.Button(main_page_window, text = "Fill", style='TButton')
